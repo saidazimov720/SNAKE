94fe833452f88c65b8c49089d6c1da3a2522974a
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-03 15:03:09 +0500

    control logic

--- snake1.py
+++ snake1.py
@@ -77,4 +77,22 @@ def gameloop():
                     if event.key == pygame.K_c:
                         gameloop()
 
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        game_over = True
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            x1_change = -snake_block
+            y1_change = 0
+        elif event.key == pygame.K_RIGHT:
+            x1_change = snake_block
+            y1_change = 0
+        elif event.key == pygame.K_UP:
+            y1_change == -snake_block
+            x1_change = 0
+        elif event.key == pygame.K_DOWN:
+            y1_change = snake_block
+            x1_change = 0
+            
+
  
