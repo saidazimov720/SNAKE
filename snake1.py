3abf2194838ec6150dba5fa584e05bacd975cb48
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-03 14:34:22 +0500

    add colors

--- snake1.py
+++ snake1.py
@@ -3,3 +3,11 @@ import time
 import random
 
 pygame.init()
+
+#define colors
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
