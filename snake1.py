6ea07eb758d35d0c0c0c0540e88607d9e5498600
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-03 14:37:01 +0500

    screen

--- snake1.py
+++ snake1.py
@@ -11,3 +11,13 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+
+#set the screen width and height
+dis_width = 800
+dis_height = 600
+
+#create the screen
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake game by Nurbek')
+#set the font and size
+font_style = pygame.font.SysFont(None, 50)
