afe08742f637966a35341a51b396f71204c731e4
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-03 14:56:27 +0500

    loop_3

--- snake1.py
+++ snake1.py
@@ -65,5 +65,16 @@ def gameloop():
             dis.blit(game_over_surface, game_over_rect)
             
             time.sleep(2)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                    game_close = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameloop()
 
  
